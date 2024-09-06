50ba6843e895194383d3fe936365f8abc31c999a
KavyaKaleeswari <kavyakaleeswari754@gmail.com>
2024-09-06 10:57:49 +0530

    first commit

---
+++ page2.js
@@ -0,0 +1,30 @@
+var a = 100 //number
+a = true //boolean
+a = "hi"//string
+
+console.log("-----incase of var-----")
+var b = 100         // variable declaraion & assign the value
+console.log(b)
+
+b = 200;            //value chage
+console.log(b)
+
+var b = "bangaore"  //variable re-declaration-bad
+console.log(b)
+
+console.log("-----incase of let-----")
+let c = 500         // variable declaraion
+console.log(c)
+c = 600             //change the value
+console.log(c)
+// let c = 800         //variable re-declaration
+// console.log(c)
+
+
+console.log("-----incase of const-----")
+const x = 1000
+console.log(x)
+
+x = 1200;
+console.log(x);
+
